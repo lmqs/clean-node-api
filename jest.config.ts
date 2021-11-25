e2920182f8398ab0fe5beeb08d9d4cd5bcfa5672
Luciana Queiroz <lucianaqueiroz098@hotmail.com>
2021-11-24 22:25:28 -0300

    chore: add husky and lint staged

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  // clearMocks: true,
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  // coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
