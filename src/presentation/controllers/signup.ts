2a8de85ee9a02a489ef6d94f1fb009f3a2971ba1
Luciana Santana <luciana.santana@objective.com.br>
2021-12-06 17:48:46 -0300

    feat: signUpController retorna 400 se o email e invalido

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from '../errors/invalid-param-errors'
 import { MissingParamError } from '../errors/missing-param-errors'
 import { badRequest } from '../helpers/http-helpers'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -12,6 +20,11 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
+
     return {
       statusCode: 400,
       body: new Error('Erro genérico')
