78100655723e17d9e8eaba6d79700f83c3a497dc
Luciana Santana <luciana.santana@objective.com.br>
2021-12-06 11:35:20 -0300

    test: em signupController retorna um erro se nome nao for preenchido

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('deverá retornar 400 se nenhum nome for fornecido', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: nome'))
+  })
+})
