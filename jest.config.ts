974705b06fa7ac73792909b58f593bf3b1584035
Luciana Santana <luciana.santana@objective.com.br>
2021-12-06 23:02:42 -0300

    chore: adicionando configuraçoes ao jest e scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 export default {
-  // clearMocks: true,
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  // coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
